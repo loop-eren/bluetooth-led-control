26e432d69da21d51468859ba36457f3785734ede
Eren Aloglu <95146157+loop-eren@users.noreply.github.com>
2022-08-09 17:24:09 +0300

    Create main file

---
+++ main.ino
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+char input_char;
+const int green=2,yellow=3,white=4,red=5;
+void setup() {
+    pinMode(green,OUTPUT);
+    pinMode(yellow,OUTPUT);
+    pinMode(white,OUTPUT);
+    pinMode(red,OUTPUT);
+    Serial.begin(9600);
+   
+}
+
+void loop() {
+  while (Serial.available()>0){
+      input_char=Serial.read();
+      if(input_char=='0'){
+        digitalWrite(red,HIGH);
+        Serial.println('Red led is opened');
+      }
+      else if(input_char=='1'){
+        digitalWrite(red,LOW);
+        Serial.println('Red led is closed');
+      }  if(input_char=='2'){
+        digitalWrite(red,HIGH);
+        Serial.println('White led is opened');
+      }
+      else if(input_char=='3'){
+        digitalWrite(red,LOW);
+        Serial.println('White led is closed');
+      }  if(input_char=='4'){
+        digitalWrite(red,HIGH);
+        Serial.println('yellow led is opened');
+      }
+      else if(input_char=='5'){
+        digitalWrite(red,LOW);
+        Serial.println('yellow led is closed');
+      }  if(input_char=='6'){
+        digitalWrite(red,HIGH);
+        Serial.println('Green led is opened');
+      }
+      else if(input_char=='7'){
+        digitalWrite(red,LOW);
+        Serial.println('Green led is closed');
+      }
+ }}
